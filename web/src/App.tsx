acda45609369ed850a7913c59b0744094ede99dd
Gustavo Mazzaro <gustamazzaro@gmail.com>
2024-09-16 13:02:10 -0300

    criando primeira pagina da aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-ilustration.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Plus } from 'lucide-react'
 
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="in.orbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
-    </>
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar Meta
+      </button>
+    </div>
   )
 }
-
-export default App
